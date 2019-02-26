8a750b9cd00084f99795e434231608bb198c1597
Kiyoto Tomioka <kiyototomioka@gmail.com>
2019-02-25 21:43:34 -0800

    Edit prefix fix

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "BTCH";
+    const std::string_view addressPrefix = "btch";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "BTCH";
