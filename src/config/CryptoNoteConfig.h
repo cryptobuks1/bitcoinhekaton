6dd0408dda4eebd352ebb728573774bf1145e275
Bitcoin Hekaton <34922579+kiyototomioka@users.noreply.github.com>
2020-03-29 01:27:22 -0700

    Update Seeds

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -244,9 +244,7 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 };
 
 const char* const SEED_NODES[] = {
- "54.183.84.20:19812", //California Seed
-  "13.112.231.16:19812", //Tokyo Seed
-  "47.52.192.96:19812",  //Hong Kong Seed
-  "167.86.113.19:19812",  //Germany Seed
+ "144.91.113.228:19812", //Germany
+  "167.86.113.17:19812", //Tokyo Seed
 };
 } // CryptoNote
